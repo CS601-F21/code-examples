c71d77e2b641ced15ba54920d2a31d32c6c0e742
Sami Rollins <srollins@cs.usfca.edu>
2021-11-08 16:00:06 -0800

    update with correct dependencies

--- JDBC/src/main/java/examples/jdbc/ConnectionPoolExample.java
+++ JDBC/src/main/java/examples/jdbc/ConnectionPoolExample.java
@@ -1,12 +1,20 @@
 package examples.jdbc;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
+/**
+ * An example to demonstrate how to use a ConnectionPool
+ */
 public class ConnectionPoolExample {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        Connection connection = DBCPDataSource.getConnection();
+        try (Connection connection = DBCPDataSource.getConnection()){
+            JDBCExample.executeInsert(connection, "Jose", 9984, "jsmith", "2026-09-01");
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- JDBC/src/main/java/examples/jdbc/DBCPDataSource.java
+++ JDBC/src/main/java/examples/jdbc/DBCPDataSource.java
@@ -1,6 +1,6 @@
 package examples.jdbc;
 
-import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 import util.Config;
 import util.Utilities;
 
@@ -13,11 +13,15 @@ import java.sql.SQLException;
  */
 public class DBCPDataSource {
 
+    // Apache commons connection pool implementation
     private static BasicDataSource ds = new BasicDataSource();
 
-
+    // This code inside the static block is executed only once: the first time the class is loaded into memory.
+    // -- https://www.geeksforgeeks.org/static-blocks-in-java/
     static {
         Config config = Utilities.readConfig();
+        // TODO: do something other than exit the whole program
+        // if the config file cannot be found
         if(config == null) {
             System.exit(1);
         }
@@ -28,6 +32,11 @@ public class DBCPDataSource {
         ds.setMaxIdle(10);
     }
 
+    /**
+     * Return a Connection from the pool.
+     * @return
+     * @throws SQLException
+     */
     public static Connection getConnection() throws SQLException {
         return ds.getConnection();
     }
